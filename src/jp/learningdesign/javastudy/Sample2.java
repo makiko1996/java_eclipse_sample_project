3ab1538c0c7e39acd52cd2f366f093bea15532c7
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-02-18 18:21:36 +0900

    [add]udemy_1.27

---
+++ src/jp/learningdesign/javastudy/Sample2.java
@@ -0,0 +1,11 @@
+package jp.learningdesign.javastudy;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse!\n\'It's Tuesday today. '\n\"今日は火曜日です。\"");
+
+	}
+
+}
